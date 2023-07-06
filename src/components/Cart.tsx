142195f67dd00c16dd3c09590732bad31673090e
samssjubair <samssjubair@gmail.com>
2023-07-07 03:40:24 +0600

    calculate total cost

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -16,14 +16,9 @@ import { useAppDispatch, useAppSelector } from '@/redux/hooks';
 import { addToCart, removeFromCart, removeOneFromCart } from '@/redux/features/cart/cartSlice';
 
 export default function Cart() {
-  const {products } = useAppSelector(state=>state.cart)
+  const {products, total } = useAppSelector(state=>state.cart)
   const dispatch = useAppDispatch();
-  //! Dummy data
 
-  // const products: IProduct[] = [];
-  const total = 0;
-
-  //! **
 
   return (
     <Sheet>
--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -3,10 +3,12 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface ICart{
     products: IProduct[];
+    total: number;
 }
 
 const initialState: ICart = {
-    products: []
+    products: [],
+    total: 0
 }
 
 const cartSlice = createSlice({
@@ -21,6 +23,7 @@ const cartSlice = createSlice({
             }else{
                 state.products.push({...action.payload, quantity: 1})
             }
+            state.total+=action.payload.price;
         },
         removeOneFromCart:(state, action: PayloadAction<IProduct>) => {
             const existing = state.products.find(
@@ -33,9 +36,11 @@ const cartSlice = createSlice({
                     (product) => product._id !== action.payload._id
                 );
             } 
+            state.total -= action.payload.price;
         },
         removeFromCart:(state, action: PayloadAction<IProduct>) => {
             state.products=state.products.filter((product)=>product._id!==action.payload._id)
+            state.total -= action.payload.price* action.payload.quantity!;
         }
     }
 
